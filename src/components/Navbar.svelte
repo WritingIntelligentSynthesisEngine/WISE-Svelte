43ad2cbd534dc851c688b40ed77b1fe10fb05850
骑士姬 <2067834160@qq.com>
2026-01-06 04:41:00 +0800

    feat: 实现导航栏组件

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,21 @@
+<!-- src/components/Navbar.svelte -->
+<script lang="ts">
+  import NeumoButton from "./NeumoButton.svelte";
+
+  export let contents: readonly string[];
+
+  let selectedIndex: number = -1;
+
+  function handleSelect(index: number): void {
+    selectedIndex = index;
+  }
+</script>
+
+<div
+  class="fixed top-0 left-0 right-0 z-50 py-2 px-2 flex items-center gap-4 shadow-md
+         bg-white dark:bg-gray-700"
+>
+  {#each contents as content, index}
+    <NeumoButton {content} active={index === selectedIndex} onClick={() => handleSelect(index)} />
+  {/each}
+</div>
